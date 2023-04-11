3158e91a7d9f553baf2cad660cade814003dbce8
Collins Hillary <collinshillary@Collinss-MacBook-Air.local>
2023-04-11 12:09:59 +0300

    ADD:test code

---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+const request = require("supertest")
+const baseURL = "http://localhost:3000"
+
+describe("GET /", () => {
+      it("should return 200", async () => {
+        const response = await request(baseURL).get("/");
+        expect(response.statusCode).toBe(200);
+      });
+});
